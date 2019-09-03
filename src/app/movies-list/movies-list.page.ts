3796b650c92b5c69e7a9fd35440850f9c45ed922
Keisha Ding <keisha.seow@outlook.com>
2019-09-03 09:12:05 -0600

    initial branch commit

---
+++ src/app/movies-list/movies-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.page.html',
+  styleUrls: ['./movies-list.page.scss'],
+})
+export class MoviesListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

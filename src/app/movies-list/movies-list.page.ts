79dbad538f330588fcc303c94d36a376045400b1
Keisha Ding <keisha.seow@outlook.com>
2019-09-03 22:10:46 -0600

    2nd commit, added basics to MovieList & loading indicator

--- src/app/movies-list/movies-list.page.ts
+++ src/app/movies-list/movies-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoadingController, NavController} from '@ionic/angular';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-movies-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesListPage implements OnInit {
 
-  constructor() { }
+  private movies: Movies[];
+  constructor(
+      private movieApiService: MovieApiService,
+      private loader: LoadingController,
+      private navCrtl: NavController,
+      private route: ActivatedRoute,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const loading = await this.loader.create({
+      message: 'Getting movies...'
+    });
+    loading.present().then(() => {
+      const selectedMovieId = this.route.snapshot.paramMap.get('id');
+      // TODO: getMovieData needs to match API function
+      this.movieApiService.getMovieData(selectedMovieId).subscribe(data => {
+        this.movies = data.movies;
+
+        console.log('movies: ', this.movies);
+        console.log(data, this.movies);
+        loading.dismiss();
+      });
+    });
   }
-
 }

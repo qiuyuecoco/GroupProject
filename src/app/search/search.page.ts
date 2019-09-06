3eb85e32e3e44da676410a49016dd9a93d5222af
David Schlenker <ironinstrumentals@gmail.com>
2019-09-05 22:19:39 -0600

    Load Movies on Init

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -12,33 +12,33 @@ export class SearchPage implements OnInit {
   movies = {
     results: ['']
   };
-  filtered = [];
+  filtered: any[] = [];
   constructor(private movieAPI: MovieApiService) { }
 
   ngOnInit() {
     this.getMovie();
-    // @ts-ignore
   }
   searchByName() {
-    console.log('Search by Name');
     if (document.getElementById('searchBar')) {
       // @ts-ignore
-      const Query = document.getElementById('searchBar').value.toLowerCase();
+      const Query = document.getElementById('searchBar').value;
       this.filtered = [];
       // @ts-ignore
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < this.movies.results.length; i++) {
-        // @ts-ignore
-        console.log(this.movies.results[i]);
-        // @ts-ignore
-        if (this.movies.results[i].title.toLowerCase().includes(Query)) {
+      if (Query !== undefined) {
+// tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.movies.results.length; i++) {
           // @ts-ignore
-          this.filtered.push(this.movies.results[i]);
-        } else {
-          if (Query === '' || Query === null || Query === undefined) {
-            this.resetQuery();
+          if (this.movies.results[i].title.toLowerCase().includes(Query.toLowerCase())) {
+            // @ts-ignore
+            this.filtered.push(this.movies.results[i]);
+          } else {
+            if (Query === '' || Query === null || Query === undefined) {
+              this.resetQuery();
+            }
           }
         }
+      } else {
+        this.filtered = this.movies.results;
       }
     }
   }
@@ -47,8 +47,10 @@ export class SearchPage implements OnInit {
   }
   getMovie() {
     // @ts-ignore
-    this.movieAPI.dynamicMovieTypes('movie', 'top_rated').subscribe(data => { this.movies = data; console.log(data.results); });
+    // tslint:disable-next-line:max-line-length
+    this.movieAPI.dynamicMovieTypes('movie', 'top_rated').subscribe(data => { this.movies = data; console.log(data.results); this.filtered = this.movies.results; });
     this.resetQuery();
+    this.filtered = this.movies.results;
     // this.filtered = this.movies;
     // this.movieAPI.dynamicMovieSearch('movie', document.getElementById('searchBar').value.toLowerCase(), '&language=en-US');
   }

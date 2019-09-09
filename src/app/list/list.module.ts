dc76dc45288fac6382751e3838c312776bb1868c
Keisha Ding <keisha.seow@outlook.com>
2019-09-09 09:55:09 -0600

    Watch List is 2 sections, part2

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -2,42 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { ListPage } from './list.page';
 
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
+const routes: Routes = [
+  {
+    path: '',
+    component: ListPage,
+    children: [
       {
-        path: '',
-        component: ListPage,
+        path: 'watch-list',
         children: [
           {
-            path: 'watch-list',
-            children: [
-              {
-                path: '',
-                loadChildren: './watch-list/watch-list.module#WatchListPageModule'
-              }
-            ]
-          },
-          {
-            path: 'watched-list',
-            children: [
-              {
-                path: '',
-                loadChildren: './watched-list/watched-list.module#WatchedListPageModule'
-              }
-            ]
-          },
+            path: '',
+            loadChildren: './watch-list/watch-list.module#WatchListPageModule'
+          }
+        ]
+      },
+      {
+        path: 'watched-list',
+        children: [
           {
             path: '',
-            redirectTo: '/list/watch-list',
-            pathMatch: 'full'
+            loadChildren: './watched-list/watched-list.module#WatchedListPageModule'
           }
         ]
       },
@@ -46,7 +34,20 @@ import { ListPage } from './list.page';
         redirectTo: '/list/watch-list',
         pathMatch: 'full'
       }
-    ])
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/list/watch-list',
+    pathMatch: 'full'
+  }
+]
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [ListPage]
 })

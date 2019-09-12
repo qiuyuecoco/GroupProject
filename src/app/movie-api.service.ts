31df9a5c1821c4b147de07f412700eb1302303c2
Keisha Ding <keisha.seow@outlook.com>
2019-09-12 08:00:26 -0600

    pushing changes to sync changes

--- src/app/movie-api.service.ts
+++ src/app/movie-api.service.ts
@@ -39,12 +39,6 @@ export class MovieApiService {
     return this.http.get(url).pipe(map(data => data)
     );
   }
-  dynamicMovieSearch(type, query, language): Observable<any> {
-    // tslint:disable-next-line:max-line-length
-    const url = `https://api.themoviedb.org/3/search/${type}?api_key=4eb5c031eab630e105a371a7a7c4488e${query}${language}&page=1&include_adult=false&`;
-    return this.http.get(url).pipe(map(data => data));
-
-  }
 
   getPopularMovies(): Observable<Movies[]> {
     return this.http.get<Movies[]>(environment.popularMovieUrl);
